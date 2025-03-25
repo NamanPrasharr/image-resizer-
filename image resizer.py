cd3b26308c1c6c08c1cfb6f19b62a52b6d7107ad
NamanPrasharr <namanpra31535@gmail.com>
2025-03-25 16:18:24 +0600

    Image resizer.py

--- image resizer.py
+++ image resizer.py
@@ -4,13 +4,17 @@ import cv2
 
 # add your image name and format
 
-image = cv2.imread("Your image name.format")
+image = cv2.imread("your file name . format")
+
+cv2.imshow("tittle", image)
 
 # size you want to resize your image to
 
 width = 200
-height = 300
+height = 3000
 
 resized_image = cv2.resize(image, (width,height)) 
 
 cv2.imwrite("resized_image.jpg",resized_image)
+
+cv2.waitKey(0)
